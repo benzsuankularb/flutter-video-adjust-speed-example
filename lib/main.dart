3e6125d399e726d373f6ea75c721e2fc1899a955
Krittayot (Benz) Techasombooranakit <ktechasombooranakit@palo-it.com>
2020-04-25 10:39:36 +0700

    ADD: Example main.dart

---
+++ lib/main.dart
@@ -0,0 +1,163 @@
+import 'package:video_player/video_player.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(VideoApp());
+
+class VideoApp extends StatefulWidget {
+  @override
+  _VideoAppState createState() => _VideoAppState();
+}
+
+class _VideoAppState extends State<VideoApp> {
+  VideoPlayerController _controller;
+
+  final List<String> codecOptions = [".m3u8", ".mp4"];
+  final List<String> codecSources = [
+    "https://mnmedias.api.telequebec.tv/m3u8/29880.m3u8",
+    "https://file-examples.com/wp-content/uploads/2017/04/file_example_MP4_1920_18MG.mp4",
+  ];
+  int currentCodecIndex = 0;
+
+  final List<String> speedOptions = ["x1.0", "x.1.5", "x2.0"];
+  final List<double> speedValues = [1.0, 1.5, 2.0];
+  int currentSpeedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    final controller = VideoPlayerController.network(
+      codecSources[currentCodecIndex],
+    );
+    controller.addListener(handleControllerChanged);
+    controller.initialize().then((_) async {
+      await controller.play();
+      setState(() {
+        _controller = controller;
+      });
+    });
+  }
+
+  Future<void> stepController() async {
+    final oldController = _controller;
+    setState(() {
+      _controller = null;
+    });
+    oldController.removeListener(handleControllerChanged);
+    await oldController.pause();
+    await oldController.dispose();
+
+    currentCodecIndex = (currentCodecIndex + 1) % codecOptions.length;
+    currentSpeedIndex = 0;
+    final newController = VideoPlayerController.network(
+      codecSources[currentCodecIndex],
+    );
+    newController.addListener(handleControllerChanged);
+    await newController.initialize();
+    await newController.play();
+    setState(() {
+      _controller = newController;
+    });
+  }
+
+  void handleControllerChanged() {
+    setState(() {});
+  }
+
+  Future<void> stepSpeed() async {
+    // Comment this out if no speed functionality
+    currentSpeedIndex = (currentSpeedIndex + 1) % speedOptions.length;
+    final speedValue = speedValues[currentSpeedIndex];
+    _controller.setSpeed(speedValue);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Video Demo',
+      home: Scaffold(
+        body: SafeArea(
+          child: _controller == null
+              ? Container()
+              : Column(
+                  children: [
+                    Center(
+                      child: _controller.value.initialized
+                          ? AspectRatio(
+                              aspectRatio: _controller.value.aspectRatio,
+                              child: VideoPlayer(_controller),
+                            )
+                          : Container(),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: Wrap(
+                        children: [
+                          FlatButton(
+                            child: Text(
+                              _controller.value.isPlaying ? "pause" : "play",
+                            ),
+                            onPressed: () {
+                              if (_controller.value.isPlaying) {
+                                _controller.pause();
+                              } else {
+                                _controller.play();
+                              }
+                            },
+                            color: Colors.blue,
+                          ),
+                          SizedBox(width: 10),
+                          FlatButton(
+                            child: Text("<<"),
+                            onPressed: () async {
+                              final currentPosition =
+                                  await _controller.position;
+                              await _controller.seekTo(
+                                currentPosition - Duration(seconds: 10),
+                              );
+                            },
+                            color: Colors.blue,
+                          ),
+                          SizedBox(width: 10),
+                          FlatButton(
+                            child: Text(">>"),
+                            onPressed: () async {
+                              final currentPosition =
+                                  await _controller.position;
+                              await _controller.seekTo(
+                                currentPosition + Duration(seconds: 10),
+                              );
+                            },
+                            color: Colors.blue,
+                          ),
+                          SizedBox(width: 10),
+                          FlatButton(
+                            child: Text(
+                              "Switch source (currently ${codecOptions[currentCodecIndex]})",
+                            ),
+                            onPressed: stepController,
+                            color: Colors.amber,
+                          ),
+                          SizedBox(width: 10),
+                          FlatButton(
+                            child: Text(
+                              "Switch speed (currently ${speedOptions[currentSpeedIndex]})",
+                            ),
+                            onPressed: stepSpeed,
+                            color: Colors.amber,
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
